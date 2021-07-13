ab4d316491da80ac55746247d676e7bea0b73da7
tkhrttr <>
2021-07-13 22:40:58 +0900

    delete commentout

--- src/main/kotlin/todo/Todo.kt
+++ src/main/kotlin/todo/Todo.kt
@@ -3,8 +3,6 @@ package todo
 class Todo(
     val id: Int,
     val title: String,
-//    val list: List<String> = listOf("a", "b", "c")
-//    val list: String = "a"
 ) {
     fun getList(): List<Any> {
         return listOf(id, title)
